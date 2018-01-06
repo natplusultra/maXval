cb107d8b9c1ed95c073a911d4f933d30657b3b54
Natalie Loman <natalie4@gmail.com>
2018-01-06 12:15:59 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes");     uncomment this when routes are ready
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// configures body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// serves up static assets
+app.use(express.static("client/build"));
+
+// adds the routes
+// app.use(routes);     uncomment this when routes are ready
+
+// sets up promises with mongoose
+mongoose.Promise = global.Promise;
+// connects to the mongo db
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/maxval");
+
+// starts the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}.`);
+});
