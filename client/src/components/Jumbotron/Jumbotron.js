1d4bedd585a077441a520ad48e9bf2e0d161232a
Natalie Loman <natalie4@gmail.com>
2018-01-07 00:40:34 -0800

    added jumbotron image and styling

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Jumbotron.css";
 
 const Jumbotron = () =>
   <div className="jumbotron">
-    <h1>maXval</h1>
+    <h1 className="title">maXval</h1>
     <br />
-    <p>A crowdsourced valuation tool for your products.</p>
+    <p className="tagline">A crowdsourced valuation tool for your products.</p>
+    <br />
     <br />
     <div className="btn-div">
-        <a className="waves-effect waves-light btn center-align">Get started!</a>
+        <Link to="/rate"><a className="waves-effect waves-light btn center-align">Get started!</a></Link>
     </div>
     
   </div>;
