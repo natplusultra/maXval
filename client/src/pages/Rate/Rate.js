5d7a1ae345f4c0be159ef5aa9c38518915f787be
Utubor <utubor@hpe.com>
2018-01-18 19:28:05 -0800

    Rate first pass

--- client/src/pages/Rate/Rate.js
+++ client/src/pages/Rate/Rate.js
@@ -6,46 +6,24 @@ import "./Rate.css";
 
 class Rate extends Component {
     state = {
-        // ###### TEST DATA ######
-        products: [
-            {
-                title: "Artisanal Wooden Bowls",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                image: "https://images.unsplash.com/photo-1501420896719-ad7fe0ee297e?auto=format&fit=crop&w=2550&q=80",
-                userName: "Bobby Bob",
-                userLocation: "San Diego",
-                userImage: "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?auto=format&fit=crop&w=1302&q=80"
-            },
-            {
-                title: "Handmade Ceramic Vases",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                image: "https://images.unsplash.com/photo-1481401908818-600b7a676c0d?auto=format&fit=crop&w=2551&q=80",
-                userName: "Jane Doe",
-                userLocation: "New York",
-                userImage: "https://images.unsplash.com/photo-1506863530036-1efeddceb993?auto=format&fit=crop&w=2141&q=80"
-            },
-            {
-                title: "Custom Leather Shoes",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                image: "https://images.unsplash.com/photo-1499013819532-e4ff41b00669?auto=format&fit=crop&w=1600&q=80",
-                userName: "Mr. Shoe",
-                userLocation: "Los Angeles",
-                userImage: "https://images.unsplash.com/photo-1502980426475-b83966705988?auto=format&fit=crop&w=1273&q=80"
-            }
-        ]
+        products: [],
+        prodIndex: 0
+        
     }
 
-    // loads products
-    // componentDidMount() {
-    //     this.loadProducts();
-    // }
+    //loads products
+    componentDidMount() {
+        this.loadProducts();
+    }
 
-    // loads all products in the database
-    // loadProducts = () => {
-    //     API.getAllItems()
-    //     .then(res => this.setState({ products: res.data }))
-    //     .catch(err => console.log(err));
-    // };
+    //loads all products in the database
+    loadProducts = () => {
+        API.getAllItems()
+        .then(res => {
+            console.log(res)
+            this.setState({ products: res.data })})
+        .catch(err => console.log(err));
+    };
 
     render() {
         console.log(this.props.user);
@@ -55,11 +33,11 @@ class Rate extends Component {
                     <div className="row productDiv">
                         <div className="col s12 l6">             
                             <ProductCard 
-                                productTitle={product.title}
+                                productTitle={product.name}
                                 productDesc={product.description}
-                                productImage={product.image}
+                                productImage={product.img}
                                 userName={product.userName}
-                                userLocation={product.userLocation}
+                                userLocation={product.location}
                                 userImage={product.userImage}
                                 style={{display: "inline-block"}}
                             />
