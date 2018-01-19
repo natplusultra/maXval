d7eb5fc34d58dc27080ca46153c14dcffcbfacd4
Jordan Lapid <jordanlapid@gmail.com>
2018-01-18 23:20:17 -0800

    fixed sliders and state on rate page

--- client/src/components/RatingPanel/RatingPanel.js
+++ client/src/components/RatingPanel/RatingPanel.js
@@ -1,97 +1,114 @@
-import React from "react";
+import React, { Component } from "react";
 import {Card, CardTitle, CardText} from "material-ui/Card";
-// import SliderCard from "../SliderCard";
 import TextField from "material-ui/TextField";
 import "./RatingPanel.css";
 import Slider from 'material-ui/Slider';
 
 
+class RatingPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.handleQualitySlider = this.handleQualitySlider.bind(this);
+        this.handleAppealSlider = this.handleAppealSlider.bind(this);
+        this.handleValueSlider = this.handleValueSlider.bind(this);
+        console.log(props);
+    }
 
+    handleQualitySlider(event, value){
+        this.props.handleQualitySlider(value);
+    }
 
+    handleAppealSlider(event, value){
+        this.props.handleAppealSlider(value);
+    }
 
-const RatingPanel = () => {
+    handleValueSlider(event, value){
+        this.props.handleValueSlider(value);
+    }
 
-    // handleSlider = (event, value) => {
-    //     this.setState({sliderValue: value});
-    // };
-
-    return (
-        <div>
-            <Card>
-                <CardTitle 
-                    title="Quality" 
-                    style={{padding: "20px 20px 0px 20px"}} 
-                />
-                <CardText style={{padding: "0px 20px 10px 20px"}}>
-                    <div>
-                        <Slider
-                            min={0}
-                            max={10}
-                            step={1}
-                            // value={this.state.sliderValue}
-                            // onChange={this.handleSlider}
-                            style={{margin: "0px"}}
-                        />
-                        <p>
-                            <span>{"Rating: "}</span>
-                            {/* <span>{this.state.sliderValue}</span> */}
-                            <span>{" from a range of 0 to 10."}</span>
-                        </p>
-                    </div>
-                </CardText>
-            </Card>
-            <Card>
-                <CardTitle 
-                    title="Appeal" 
-                    style={{padding: "20px 20px 0px 20px"}} 
-                />
-                <CardText style={{padding: "0px 20px 10px 20px"}}>
-                    <div>
-                        <Slider
-                            min={0}
-                            max={10}
-                            step={1}
-                            // value={this.state.sliderValue}
-                            // onChange={this.handleSlider}
-                            style={{margin: "0px"}}
-                        />
-                        <p>
-                            <span>{"Rating: "}</span>
-                            {/* <span>{this.state.sliderValue}</span> */}
-                            <span>{" from a range of 0 to 10."}</span>
-                        </p>
-                    </div>
-                </CardText>
-            </Card>
-            <Card>
-                <CardTitle 
-                    title="Value" 
-                    style={{padding: "20px 20px 0px 20px"}} 
-                />
-                <CardText style={{padding: "0px 20px 10px 20px"}}>
-                    What is the maximum you would pay for this product?
-                    <br/>
-                    <div>
-                        <Slider
-                            min={0}
-                            max={10}
-                            step={1}
-                            // value={this.state.sliderValue}
-                            // onChange={this.handleSlider}
-                            style={{margin: "0px"}}
-                        />
-                        <p>
-                            <span>{"Rating: "}</span>
-                            {/* <span>{this.state.sliderValue}</span> */}
-                            <span>{" from a range of 0 to 10."}</span>
-                        </p>
-                    </div>
-                    <br/>
-                    <a className="waves-effect waves-light btn">Submit Rating</a>
-                </CardText>
-            </Card>
-        </div>
-    )
+    render () {
+        return (
+            <div>
+                <Card>
+                    <CardTitle 
+                        title="Quality" 
+                        style={{padding: "20px 20px 0px 20px"}} 
+                    />
+                    <CardText style={{padding: "0px 20px 10px 20px"}}>
+                        <div>
+                            <Slider
+                                name="qualitySlider"
+                                min={0}
+                                max={10}
+                                step={1}
+                                value={this.props.qualitySlider}
+                                onChange={this.handleQualitySlider}
+                                style={{margin: "0px"}}
+                            />
+                            <p>
+                                <span>{"Rating: "}</span>
+                                <span>{this.props.qualitySlider}</span>
+                                <span>{" from a range of 0 to 10."}</span>
+                            </p>
+                        </div>
+                    </CardText>
+                </Card>
+                <Card>
+                    <CardTitle 
+                        title="Appeal" 
+                        style={{padding: "20px 20px 0px 20px"}} 
+                    />
+                    <CardText style={{padding: "0px 20px 10px 20px"}}>
+                        <div>
+                            <Slider
+                                name="appealSlider"
+                                min={0}
+                                max={10}
+                                step={1}
+                                value={this.props.appealSlider}
+                                onChange={this.handleAppealSlider}
+                                style={{margin: "0px"}}
+                            />
+                            <p>
+                                <span>{"Rating: "}</span>
+                                <span>{this.props.appealSlider}</span>
+                                <span>{" from a range of 0 to 10."}</span>
+                            </p>
+                        </div>
+                    </CardText>
+                </Card>
+                <Card>
+                    <CardTitle 
+                        title="Value" 
+                        style={{padding: "20px 20px 0px 20px"}} 
+                    />
+                    <CardText style={{padding: "0px 20px 10px 20px"}}>
+                        What is the maximum you would pay for this product?
+                        <br/>
+                        <div>
+                            <Slider
+                                name="valueSlider"
+                                min={0}
+                                max={10}
+                                step={1}
+                                value={this.props.valueSlider}
+                                onChange={this.handleValueSlider}
+                                style={{margin: "0px"}}
+                            />
+                            <p>
+                                <span>{"Rating: "}</span>
+                                <span>{this.props.valueSlider}</span>
+                                <span>{" from a range of 0 to 10."}</span>
+                            </p>
+                        </div>
+                        <br/>
+                        <a className="waves-effect waves-light btn">Submit Rating</a>
+                    </CardText>
+                </Card>
+            </div>
+        );
+    }
 }
+
 
 export default RatingPanel;
