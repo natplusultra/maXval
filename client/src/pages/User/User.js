5880090394112b7015aa8d42dda15be34339d9ff
Natalie Loman <natalie4@gmail.com>
2018-01-19 22:24:34 -0800

    added code to pull product id and allow click of a product to route you to that product page with id

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -2,37 +2,13 @@ import React, { Component } from "react";
 import Avatar from "material-ui/Avatar";
 import ProductCard from "../../components/ProductCard";
 import ProductUpload from "../../components/ProductUpload";
+import { Link } from "react-router-dom";
 import API from "../../utils/API";
 import "./User.css";
 
 class User extends Component {
     state = {
-        products: [
-            // {
-            //     title: "Handmade Ceramic Vases 1",
-            //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            //     image: "https://images.unsplash.com/photo-1481401908818-600b7a676c0d?auto=format&fit=crop&w=2551&q=80",
-            //     userName: "Jane Doe",
-            //     userLocation: "New York",
-            //     userImage: "https://images.unsplash.com/photo-1506863530036-1efeddceb993?auto=format&fit=crop&w=2141&q=80"
-            // },
-            // {
-            //     title: "Handmade Ceramic Vases 2",
-            //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            //     image: "https://images.unsplash.com/photo-1481401908818-600b7a676c0d?auto=format&fit=crop&w=2551&q=80",
-            //     userName: "Jane Doe",
-            //     userLocation: "New York",
-            //     userImage: "https://images.unsplash.com/photo-1506863530036-1efeddceb993?auto=format&fit=crop&w=2141&q=80"
-            // },
-            // {
-            //     title: "Handmade Ceramic Vases 3",
-            //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            //     image: "https://images.unsplash.com/photo-1481401908818-600b7a676c0d?auto=format&fit=crop&w=2551&q=80",
-            //     userName: "Jane Doe",
-            //     userLocation: "New York",
-            //     userImage: "https://images.unsplash.com/photo-1506863530036-1efeddceb993?auto=format&fit=crop&w=2141&q=80"
-            // }
-        ]
+        products: []
     }
 
     // loads user's products
@@ -66,14 +42,18 @@ class User extends Component {
                     <p>Click on a product to view ratings and stats.</p>
                 </div>
                 <div className="row grid-div">
-                    {this.state.products.map(product => (
-                        <ProductCard 
-                            productTitle={product.name}
-                            productDesc={product.description}
-                            productImage={product.img}
-                            userLocation={product.location}
-                            style={{display: "inline"}}
-                        />
+                    {this.state.products.map((product, i) => (
+                        <a href={`/product/${product._id}`} className="product-card">
+                            <ProductCard 
+                                key={i}
+                                productTitle={product.name}
+                                productDesc={product.description}
+                                productImage={product.img}
+                                userLocation={product.location}
+                                productID={product._id}
+                                style={{display: "inline"}}
+                            />
+                        </a>
                     ))}
                 </div>
             </div>
