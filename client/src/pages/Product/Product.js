31cc4013349cccc1ebdb1fa16755f169f7d22b29
Utubor <utubor@hpe.com>
2018-01-24 18:23:17 -0800

    More product page changes

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Product.css";
 import ProductCard from "../../components/ProductCard";
-import {Bar, Line} from 'react-chartjs-2';
+import {Bar, Line, HorizontalBar} from 'react-chartjs-2';
 import MenuItem from 'material-ui/MenuItem';
 import Card from 'material-ui/Card'
 import API from "../../utils/API";
@@ -21,15 +21,26 @@ class Dashboard extends Component {
       dropdownOpen: false,
       radioSelected: 2,
       reviews: [],
+      //itemId: this.props.match.params.id
       itemId: this.props.match.params.id,
-      // itemId: "5a5e6d2ce738f254a0b2a594",
+      XAxis: ["0", '1', '2', '3', '4', '5', '6', '7', "8", "9", "10"],
+
       qualityRawData: [],
-      qualityXAxix: [],
-      qualityYAxix: [],
+      qualityYAxis: [],
       qualityAvg: 0,
       qualityMedian: 0,
-      appealData: [],
-      valueData:[]
+
+
+      appealRawData: [],
+      appealYAxis: [],
+      appealAvg: 0,
+      appealMedian: 0,
+
+      valueRawData: [],
+      valueYAxis: [],
+      valueAvg: 0,
+      valueMedian: 0,
+
     }
 
       // loads user's products
@@ -37,54 +48,88 @@ class Dashboard extends Component {
         this.loadReviews(this.state.itemId);
     }
 
+    //Load reviews from API, and parse the reviews data
     loadReviews = (itemId) => {
+        //API call to grab the reviews based on item ID
         API.getItemReview(itemId) 
         .then(res => {
+            //Parse the reviews object into separate arrays
             this.parseReviewData(res.data)
+            //set the reviews state.
             this.setState({ reviews: res.data })})
         .catch(err => console.log(err));
     };
 
     parseReviewData(arr) {
+        //Arrays that will hold the individually parsed review data
         let quality = [];
         let appeal = [];
         let value = [];
 
+        //loop through the reviews object, and create new arrays holding quality, appeal and value
         for (let i = 0; i < arr.length; i++){
             appeal.push(arr[i].appeal);
             quality.push(arr[i].quality);
             value.push(arr[i].value);
         }
-
-        this.setState({ qualityRawData: quality });
-
+        
+        //Count the occurrences of each value.
         let qualityCount = (this.countOccurrence(quality))
-        console.log(qualityCount[0])
-        console.log(qualityCount[1])
-        this.setState({ qualityXAxix: qualityCount[0] });
-        this.setState({ qualityYAxix: qualityCount[1] });
-        this.setState({ qualityAvg: this.calAverage(quality) });
-        this.setState({ qualityMedian: this.calMedian(quality)});
-        console.log(this.state.qualityRawData)
+        let appealCount = (this.countOccurrence(appeal))
+        let valueCount = (this.countOccurrence(value))
 
+        this.setState({
+          //Quality data 
+          qualityRawData: quality,
+          qualityYAxis: qualityCount,
+          qualityAvg: this.calAverage(quality),
+          qualityMedian: this.calMedian(quality),
 
-        this.setState({ appealData: appeal });
-        this.setState({ valueData: value });
+          //Appeal data
+          appealRawData: appeal,
+          appealYAxis: appealCount,
+          appealAvg: this.calAverage(appeal),
+          appealMedian: this.calMedian(appeal),
+
+          //Value data
+          valueRawData: value,
+          valueYAxis: valueCount,
+          valueAvg: this.calAverage(value),
+          valueMedian: this.calMedian(value)
+        });
     }
 
     countOccurrence(arr) {
-      var a = [], b = [], prev;
-      arr.sort();
-      for ( var i = 0; i < arr.length; i++ ) {
-          if ( arr[i] !== prev ) {
-              a.push(arr[i]);
-              b.push(1);
-          } else {
-              b[b.length-1]++;
-          }
-          prev = arr[i];
+      arr.sort((a, b) => a - b)
+      var counts = {
+        '0':0,
+        '1':0,
+        '2':0,
+        '3':0,
+        '4':0,
+        '5':0,
+        '6':0,
+        '7':0,
+        '8':0,
+        '9':0,
+        '10':0
+      };
+
+      //Create an object with keys of "counts", and 
+      //value of the count of each number in the reviews
+      for (var i = 0; i < arr.length; i++) {
+        var num = arr[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
       }
-      return [a, b];
+
+      //Parse the counts into an array holding
+      //The Y Axis data
+      let temp = [];
+        for (var key in counts){
+          temp.push(counts[key])
+        }
+
+      return temp;
     }
 
     calAverage(arr){
@@ -101,13 +146,9 @@ class Dashboard extends Component {
       return median;
     }
 
-
-
-
-
   render() {
-    let qualityChartData = {
-      labels: this.state.qualityXAxix,
+    let chartData = {
+      labels: this.state.XAxis,
       datasets: [
         {
           label: 'Quality',
@@ -134,37 +175,62 @@ class Dashboard extends Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: this.state.qualityYAxix
+          data: this.state.qualityYAxis
+        },
+        {
+          label: 'Appeal',
+          fill: true,
+          borderColor: "#3e95cd",
+          fillColor: "#3e95cd",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: this.state.appealYAxis
+        },
+        {
+          label: 'Value',
+          borderColor: "#c45850",
+          fill: true,
+          data: this.state.valueYAxis
         }
       ],
+    };
+
+    const horizontalBarData = {
+      labels: ['Quality', 'Appeal', 'Value'],
+      datasets: [
+        {
+          label: 'Averages',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 2,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [this.state.qualityAvg, this.state.appealAvg, this.state.valueAvg]
+        }
+      ]
     };
 
 
      return (
       <div className="animated fadeIn">
         <div className="row">
-
-           <div className="col s6 l3">
-          
-            <Card className="text-white bg-primary">
-              
-               
-                <p className="mb-0">Max Quality: {Math.max(...this.state.qualityRawData)}</p>
-                <p className="mb-0">Min Quality: {Math.min(...this.state.qualityRawData)}</p>
-                <p className="mb-0">Average Quality: {this.state.qualityAvg.toFixed(1)}</p>
-                <p className="mb-0">Median Quality: {this.state.qualityMedian.toFixed(1)}</p>
-                <div className="chart-wrapper px-3" style={{height:'300px'}}>
-                  <Line data={qualityChartData} />
-                </div>
-
-                
-            </Card>
+           <div className="col s6 l3">          
+              <div className="chart-wrapper px-3" style={{height:'300px'}, {width:'600px'} }>
+                <Line data={chartData} />
+              </div>               
            </div>
-
+        </div>
+        <div className="row">
+          <div className="col s6 l3">
+               <HorizontalBar data= {horizontalBarData} />
+           </div>
         </div>
       </div>
       )
     }
   }
 
-export default Dashboard;
+export default Dashboard
